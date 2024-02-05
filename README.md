8488442d2f0ad77a501ad7a365b7d83617b3653a
eli <ellizzabbetth@gmail.com>
2024-02-05 06:49:50 -0500

    two estimators work

--- README.md
+++ README.md
@@ -41,4 +41,34 @@ Remember that your README should:
 **TODO (Optional):** This is where you can provide information about any standout suggestions that you have attempted.
 
 
+
+## Standout Suggestions
+Here are some of the suggestions to improve your project:
+
+
+Package Your Model: Can you package your model as a Docker Container so that it can be easily deployed
+Multi-Model Endpoint: Can you finetune multiple (different) pretrained models and try to deploy them to the same endpoint in the form of a Multi-Model Endpoint?
+Batch Transform: Can you create a batch transform that performs inference on the whole test set together?
+Model Explainability: Can you use Amazon Sagemaker Clarity to make your models more interpretable?
+
 ## Directions
+
+
+
+I understand your situation and I experienced the same thing before. Please note that you won't need to re-download it to your sagemaker workspace once you finished the upload.
+
+Try NOT to explore the data files using the UI, it will make everything slow.
+Delete the dataset in your workspace once you have a copy on your s3 bucket.
+
+s3://sagemaker-us-east-1-631507419627/dog-image-data
+
+
+Data Channels: the notebook parameters can be passed to your script using environment variables
+
+## Thoughts
+
+Although this is not explained, or even mentioned, on the "Step 2: Train the Model" page, in train_and_deploy.ipynb it has us training a first model using hpo.py, and then using the best parameters to train a second model using train_model.py.
+
+It looks like we could (if not for the project's requirements) just use tuner.best_estimator() from the first model that was trained using hyperparameter tuning, and not need to train the second model with best_estimator.hyperparameters(), is that correct?
+Is the purpose of training a second model, using train_model.py and best_estimator.hyperparameters() from the first training, so that we can apply model debugging and profiling to the second model training?
+I'm presuming that we would not want to debug/profile when using hyperparameter tuning, as we would not want to see all of that, from every combination it tried, is that why we're doing it this way?
--- hpo.py
+++ hpo.py
@@ -1,5 +1,3 @@
-#TODO: Import your dependencies.
-#For instance, below are some dependencies you might need if you are using Pytorch
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,71 +7,131 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 
 import argparse
+import logging
+import os
+import sys
 
-def test(model, test_loader):
-    '''
-    TODO: Complete this function that can take a model and a 
-          testing data loader and will get the test accuray/loss of the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
-    pass
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-def train(model, train_loader, criterion, optimizer):
-    '''
-    TODO: Complete this function that can take a model and
-          data loaders for training and will get train the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
-    pass
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+
+def test(model, test_loader, criterion):
+    model.eval()
+    test_loss = 0
+    running_corrects = 0
+    
+    for inputs, labels in test_loader:
+        outputs = model(inputs)
+        test_loss += criterion(outputs, labels).item()
+        _, preds = torch.max(outputs, 1)
+        running_corrects += torch.sum(preds==labels.data).item()
+    
+    average_accuracy = running_corrects/len(test_loader.dataset)
+    average_loss = test_loss/len(test_loader.dataset)
+    logger.info(f'Test set: Average loss: {average_loss}, Accuracy: {100*average_accuracy}%')
+    
+
+def train(model, train_loader, epochs, criterion, optimizer): 
+    count = 0
+    
+    for epoch in range(epochs):
+        model.train()
+        
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            count += len(inputs)
+            if count > 400:
+                break
+            
+    return model 
     
 def net():
-    '''
-    TODO: Complete this function that initializes your model
-          Remember to use a pretrained model
-    '''
-    pass
+    model = models.resnet50(pretrained = True)
+    
+    for param in model.parameters():
+        param.required_grad = False 
+    
+    num_features = model.fc.in_features
+    num_classes = 133
+    model.fc = nn.Sequential(nn.Linear(num_features, 256), 
+                                                nn.ReLU(),                 
+                                                nn.Linear(256, 128),
+                                                nn.ReLU(),
+                                                nn.Linear(128,  num_classes),
+                                                nn.LogSoftmax(dim=1))
+    return model
 
 def create_data_loaders(data, batch_size):
-    '''
-    This is an optional function that you may or may not need to implement
-    depending on whether you need to use data loaders or not
-    '''
-    pass
+    train_path = os.path.join(data, 'train')
+    test_path = os.path.join(data, 'test')
+    validation_path = os.path.join(data, 'valid')
+    
+    train_transform = transforms.Compose([transforms.RandomHorizontalFlip(p=0.5),
+                                                                          transforms.Resize(256),
+                                                                          transforms.Resize((224, 224)),
+                                                                          transforms.ToTensor()])
+
+    test_transform = transforms.Compose([transforms.Resize(256),
+                                                                        transforms.Resize((224, 224)),
+                                                                        transforms.ToTensor()])
+    
+    train_dataset = torchvision.datasets.ImageFolder(root=train_path, transform=train_transform)    
+    test_dataset = torchvision.datasets.ImageFolder(root=test_path, transform=test_transform)
+    validation_dataset = torchvision.datasets.ImageFolder(root=validation_path, transform=test_transform)
+    
+    train_data_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_data_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    validation_data_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=batch_size, shuffle=False)
+    
+    return train_data_loader, test_data_loader, validation_data_loader
 
 def main(args):
-    '''
-    TODO: Initialize a model by calling the net function
-    '''
+      
+    logger.info(f"Hyperparameters : LR: {args.lr},  Batch Size: {args.batch_size}, Epoch: {args.epochs}")
+    logger.info(f"Environment : Data_Dir: {args.data_dir},  Model_Dir: {args.model_dir}")
+    logger.info(f"Environment : Data_Dir: {os.environ['SM_CHANNEL_TRAIN']}, Model_Dir: {os.environ['SM_MODEL_DIR']}")
+     
     model=net()
     
-    '''
-    TODO: Create your loss and optimizer
-    '''
-    loss_criterion = None
-    optimizer = None
+    loss_criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
     
-    '''
-    TODO: Call the train function to start training your model
-    Remember that you will need to set up a way to get training data from S3
-    '''
-    model=train(model, train_loader, loss_criterion, optimizer)
+    train_data_loader, test_data_loader, _ = create_data_loaders(data=args.data_dir, batch_size=args.batch_size)
     
+    model = train(model, train_data_loader, args.epochs, loss_criterion, optimizer)
     '''
     TODO: Test the model to see its accuracy
     '''
-    test(model, test_loader, criterion)
-    
+    test(model, test_data_loader, loss_criterion)
     '''
     TODO: Save the trained model
     '''
-    torch.save(model, path)
+    logger.info("Saving the model")
+    torch.save(model.state_dict(), os.path.join(args.model_dir, 'model.pth'))
+    logger.info("Model saved")
 
+    
 if __name__=='__main__':
     parser=argparse.ArgumentParser()
-    '''
-    TODO: Specify all the hyperparameters you need to use to train your model.
-    '''
+    
+    parser.add_argument("--batch_size", type=int, default=64, metavar="N",   help="input batch size for training (default: 64)")
+  #  parser.add_argument("--test_batch_size", type=int, default=1000, metavar="N", help="input batch size for testing")
+    parser.add_argument("--epochs", type=int, default=2, metavar="N", help="number of epochs to train")
+    parser.add_argument("--lr", type=float, default=0.01, metavar="LR", help="learning rate")
+    parser.add_argument("--data_dir", type=str, default=os.environ["SM_CHANNEL_TRAIN"], help="training data path in S3")
+    parser.add_argument("--model_dir", type=str, default=os.environ["SM_MODEL_DIR"], help="location to save the model to")
     
     args=parser.parse_args()
-    
+    logging.info(f"Learning Rate: {args.lr}")
+    logging.info(f"Batch Size: {args.batch_size}")
+   # logging.info(f"Test Batch Size: {args.test_batch_size}")
+    logging.info(f"Epochs: {args.epochs}")
     main(args)
